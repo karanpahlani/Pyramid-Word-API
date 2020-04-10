d1e056e487142f9a2cfda307d18a8605876254c3
Karan Pahlani <pahlani@gmail.com>
2020-04-10 16:56:56 -0500

    implemented isPyramid method

--- services/isPyramid.js
+++ services/isPyramid.js
@@ -1,6 +1,27 @@
 const isPyramid = inputString => {
+    freqDict = {};
 
+    for (var i = 0; i < inputString.length; i++) {
+        tempChar = inputString.charAt(i)
 
-}
+        if ( tempChar in freqDict){
+            freqDict[tempChar] ++;
+        }
+        else{
+            freqDict[tempChar] = 1;
+        }
+      }
 
-modules.export = isPyramid;
+      frequencies = freqDict.values()
+      frequencies.sort()
+
+      for (var i = 1; i <= frequencies.length; i++) {
+        if (i != frequencies[i-1]){
+            return false
+        }
+
+      }
+      return true
+};
+
+module.exports = isPyramid;
